522eb8dbf39bf01f4fcbf8bee36b90df91919ea3
tanmaypatil <tany.patil77@gmail.com>
2023-09-24 00:05:11 +0530

    wrapper functions for simiilarity check

--- llmUtil.py
+++ llmUtil.py
@@ -6,5 +6,14 @@ def getCollectionToEmbed(dbName,modelName):
   db = sqlite_utils.Database(dbName)
   embedding_model = llm.get_embedding_model(modelName)
   embedding_model.key = read_properties('OPENAPI_KEY')
-  collection = llm.Collection("entries", db, model=embedding_model)
+  collection = llm.Collection("FFF2", db, model=embedding_model)
   return collection
+
+def embed(collection : llm.Collection , data : list):
+  collection.embed_multi(data,store=True)  
+
+def checkSimilar(collection : llm.Collection , searchStr : str) :
+  entries = collection.similar(searchStr,number=5) 
+  for entry in entries:
+    print(entry.id , entry.content,entry.score)
+  return entries
