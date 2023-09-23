6f1bb3a8238b5bb0016d78d56d7be3a2f4e81324
tanmaypatil <tany.patil77@gmail.com>
2023-09-24 00:41:52 +0530

    added a test case

--- test_cases.py
+++ test_cases.py
@@ -5,10 +5,4 @@ value = read_properties('OPENAPI_KEY')
 print(data)
 
 collection = getCollectionToEmbed("fff2.db","ada-002")
-embed(collection=collection,data=data)
-entries = checkSimilar(collection,"chrome")
-
-
-
-
-
+entries = checkSimilar(collection,"sign in")
