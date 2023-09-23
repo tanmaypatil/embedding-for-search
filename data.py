0a657d24380fb3f263328bfd8e0a92e5915759cd
tanmaypatil <tany.patil77@gmail.com>
2023-09-24 00:34:43 +0530

    basic test with value as "chrome" and output

---
+++ data.py
@@ -0,0 +1,12 @@
+data = [
+    ("test-id", "Test010BlocksAndModels"),
+    ("test-scenario" , "verify that taks created when creating a task under an existing project"),
+    ("test-action-1","go to url"),
+    ("test-action-2","login to open project"),
+    ("test-action-3","navigate to work packages"),
+    ("test-action-4","save tasks number"),
+    ("test-action-5","search tasks"),
+    ("test-action-6","close browser"),
+    ("test-expected-results","verify that value of TaskAfter is greater than TaskBefore")
+
+]
--- embed.py
+++ embed.py
@@ -1,4 +1,13 @@
 from readProp import * 
-
+from data import *
+from llmUtil import *
 value = read_properties('OPENAPI_KEY')
-print(value)
+print(data)
+
+collection = getCollectionToEmbed("fff2.db","ada-002")
+embed(collection=collection,data=data)
+entries = checkSimilar(collection,"chrome")
+
+
+
+
