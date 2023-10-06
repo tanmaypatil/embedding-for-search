2e01cea266b9f2148fbfe4339a767b30a3d03173
Tanmay Patil <tany.patil77@gmail.com>
2023-10-06 12:58:51 +0530

    adding notebooks

---
+++ print_test.py
@@ -0,0 +1,22 @@
+color_prefix_by_role = {
+    "system": "\033[0m",  # gray
+    "user": "\033[0m",  # gray
+    "assistant": "\033[92m",  # green
+}
+
+
+def print_messages(messages, color_prefix_by_role=color_prefix_by_role) -> None:
+    """Prints messages sent to or from GPT."""
+    for message in messages:
+        role = message["role"]
+        color_prefix = color_prefix_by_role[role]
+        content = message["content"]
+        print(f"{color_prefix}\n[{role}]\n{content}")
+        
+messages = []
+message = {
+    "role" : "user",
+    "content" : "what are best banks in india"
+}
+messages.append(message)
+print_messages(messages=messages)
